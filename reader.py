0798d92a65e8f4c42529ede6f43df6b4010f0bef
j0n4th444n <j0n4th444n@gmail.com>
2018-11-29 02:02:06 -0800

    read_web en reader pincha ok

--- reader.py
+++ reader.py
@@ -8,7 +8,7 @@ from PyPDF2 import PdfFileReader
 from docx import Document
 
 class Doc(Enum):
-    html = 1
+    web = 1
     pdf = 2
     word = 3
 
@@ -38,48 +38,51 @@ def read_pdf_in_path(path,lang):
                 text += pdf.getPage(i).extractText() + " "
             return text
 
-def read_html_in_path(data):
-    # removing js
-    while(True):
-        i = data.find('<script',0,len(data))
-        if i == -1:
-            break
-        j = data.find('</script>',i,len(data))
-        data = data.replace(data[i:j+9],' ',1)
+# ! funciona perfect
+def read_web_in_path(path):
+    with open(path,'r',encoding='utf8',errors='ignore') as f:
+        data = f.read()
+        while(True):
+            i = data.find('<script',0,len(data))
+            if i == -1:
+                break
+            j = data.find('</script>',i,len(data))
+            data = data.replace(data[i:j+9],' ',1)
 
-    # removing CSS
-    while(True):
-        i = data.find('<style',0,len(data))
-        if i == -1:
-            break
-        j = data.find('</style>',i,len(data))
-        data = data.replace(data[i:j+8],' ',1)
+        # removing CSS
+        while(True):
+            i = data.find('<style',0,len(data))
+            if i == -1:
+                break
+            j = data.find('</style>',i,len(data))
+            data = data.replace(data[i:j+8],' ',1)
 
-    # removing Comments
-    while(True):
-        i = data.find('/*',0,len(data))
-        if i == -1:
-            break
-        j = data.find('*/',i,len(data))
-        data = data.replace(data[i:j+2],' ',1)
+        # removing Comments
+        while(True):
+            i = data.find('/*',0,len(data))
+            if i == -1:
+                break
+            j = data.find('*/',i,len(data))
+            data = data.replace(data[i:j+2],' ',1)
 
-    soup = BeautifulSoup(data,"html5lib")
-    return soup.get_text(strip=True,separator=' ')
-    pass
+        soup = BeautifulSoup(data,"html5lib")
+        return soup.get_text(strip=True,separator=' ')
 
 def read_word_in_path(data):
     pass
 
-def read(path,doc_type,lang):
+def read(path,doc_type,lang=None):
     if os.path.isfile(path):
+        # ! PDF (.pdf)
         if doc_type is Doc.pdf:
             return read_pdf_in_path(path,lang)
+        # ! Word Document (.docx)
         elif doc_type is Doc.word:
             pass
+        # ! Web Page(.htm .html)
         else:
-            pass
+            return read_web_in_path(path)
 
 # print(read("C:\\Users\\jonathaaan\\Desktop\\testing\\2018-2019. Orientaciones del proyecto de SI.pdf",Doc.pdf,Lang.ES))
 # print(read("C:\\Users\\jonathaaan\\Desktop\\testing\\to_print-mining-the-news.pdf",Doc.pdf,Lang.EN))
-
-
+# print(read("C:\\Users\\jonathaaan\\Desktop\\testing\\test_ibm3.html",Doc.web))
